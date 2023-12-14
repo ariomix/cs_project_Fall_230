3499ab4ad121f9a3626abeb957c47faa96956c98
ariomix <153685927+ariomix@users.noreply.github.com>
2023-12-14 16:35:57 -0500

    Update 3Pivot_Tables_Analysis.py

--- Final_Project_Sumbission/Multipage/pages/3Pivot_Tables_Analysis.py
+++ Final_Project_Sumbission/Multipage/pages/3Pivot_Tables_Analysis.py
@@ -69,26 +69,25 @@ def plotting(pivot_table,value_column, # plotting function. Main parameters are 
              max=None, # max = maxiumum value of the plotted column if needed
              min=None): # min = minimum value of the plotted column if needed
 
-    match type: # match-case to match the coding needed for each chart type
-        case "pie": # if a pie chart, then
-            pivot_table[value_column].plot(kind=type, figsize=(8, 8),autopct='%1.1f%%') # pie chart is plotted with values as a percentage
-            plt.title(title,fontdict= font_title) # title is given
-            plt.ylabel(y_label, fontdict=font_labels) # y_label is given with the default value being none (so it is removed)
-        case "bar": # if a bar chart, then
-            plt.figure(figsize=(8,8)) # size of chart is plotted
-            plt.bar(pivot_table.index,pivot_table[value_column],color = 'Blue') # bar type is plotted with all bars being blue
-            if max != None and min != None: # Used ChatGPT. if max and min are given in the function, then
-                plt.bar(pivot_table[pivot_table[value_column] == min].index,min, color='red') # min value is plotted as red
-                plt.bar(pivot_table[pivot_table[value_column] == max].index,max, color='green') # max vlue is plotted as green
-            plt.xticks(rotation = 90) # changes the rotation of the x-axis labels
-            plt.title(title,fontdict= font_title) # plots a title
-            plt.xlabel(x_label,fontdict=font_labels)  # plots a x-axis label
-            plt.ylabel(y_label,fontdict=font_labels) # plots a y-axis label
-        case "stacked": # if stacked, then
-            pivot_table.plot(kind="barh", stacked=True) # stacked horizontal bar chart is plotted
-            plt.title(title, fontdict= font_title) # title is plotted
-            plt.ylabel(y_label,fontdict=font_labels) # y_label is plotted
-            plt.xlabel(x_label,fontdict=font_labels) # x_label is plotted
+    if type == "pie": # match-case to match the coding needed for each chart type
+        pivot_table[value_column].plot(kind=type, figsize=(8, 8),autopct='%1.1f%%') # pie chart is plotted with values as a percentage
+        plt.title(title,fontdict= font_title) # title is given
+        plt.ylabel(y_label, fontdict=font_labels) # y_label is given with the default value being none (so it is removed)
+    elif type == "bar": # if a bar chart, then
+        plt.figure(figsize=(8,8)) # size of chart is plotted
+        plt.bar(pivot_table.index,pivot_table[value_column],color = 'Blue') # bar type is plotted with all bars being blue
+        if max != None and min != None: # Used ChatGPT. if max and min are given in the function, then
+            plt.bar(pivot_table[pivot_table[value_column] == min].index,min, color='red') # min value is plotted as red
+            plt.bar(pivot_table[pivot_table[value_column] == max].index,max, color='green') # max vlue is plotted as green
+        plt.xticks(rotation = 90) # changes the rotation of the x-axis labels
+        plt.title(title,fontdict= font_title) # plots a title
+        plt.xlabel(x_label,fontdict=font_labels)  # plots a x-axis label
+        plt.ylabel(y_label,fontdict=font_labels) # plots a y-axis label
+    else: # if stacked, then
+        pivot_table.plot(kind="barh", stacked=True) # stacked horizontal bar chart is plotted
+        plt.title(title, fontdict= font_title) # title is plotted
+        plt.ylabel(y_label,fontdict=font_labels) # y_label is plotted
+        plt.xlabel(x_label,fontdict=font_labels) # x_label is plotted
     return plt # the chart is returned
 
 def main():
@@ -97,43 +96,30 @@ def main():
         options = choices) # choices listed for user
 
     df = reorder(filter_for_empty_and_useless(read_data_and_rename())) # data frame is created, filtered, and reordered (columns)
+        
+    if chosen == "Pie Graph": # if Pie Graph, then
+        pivot_vendors = df.pivot_table(values="OBJECTID", index="VENDOR", aggfunc="count") # Pivot Table of the number parking spots for each vendor type will be created
+        st.pyplot(plotting(pivot_vendors,"OBJECTID", 'pie','Major Vendors')) # everything is plotted and plotted o streamlit
 
-    match chosen: # based on the user's selection, we have code for each scenario ("Pie Graph", "Bar Chart", "Stacked Bar Chart")
+    elif chosen == "Bar Chart": # if Bar Chart is selected, then
+        pivot_streets = df.pivot_table(values="OBJECTID", index="STREET", aggfunc="count") # Number of parking lots by street
+        pivot_streets = pivot_streets[pivot_streets["OBJECTID"] >= 100] # We are filtering for streets with at least 100 parking lots (since there are a lot of streets)
+        pivot_streets = pivot_streets.sort_values(by="OBJECTID", ascending=True) # The major streets (>=100 in parking spots) are sorted in ascending order
+        high, low = min_max(pivot_streets, "OBJECTID") # The street with the highest and the lowest parking spots are retrieved
+        st.pyplot(plotting(pivot_streets, "OBJECTID",'bar','Major Streets and amount of Parking Lots', "# of parking spots by Major street",
+                        "*A Major Street is defined as having >= 100 parking spots",
+                        max = high,
+                        min = low,
+                        )) # Pivot table is plotted as a bar chart
 
-        case "Pie Graph": # if Pie Graph, then
-            pivot_vendors = df.pivot_table(values="OBJECTID", index="VENDOR", aggfunc="count") # Pivot Table of the number parking spots for each vendor type will be created
-            st.pyplot(plotting(pivot_vendors,"OBJECTID", 'pie','Major Vendors')) # everything is plotted and plotted o streamlit
-
-        case "Bar Chart": # if Bar Chart is selected, then
-            pivot_streets = df.pivot_table(values="OBJECTID", index="STREET", aggfunc="count") # Number of parking lots by street
-            pivot_streets = pivot_streets[pivot_streets["OBJECTID"] >= 100] # We are filtering for streets with at least 100 parking lots (since there are a lot of streets)
-            pivot_streets = pivot_streets.sort_values(by="OBJECTID", ascending=True) # The major streets (>=100 in parking spots) are sorted in ascending order
-            high, low = min_max(pivot_streets, "OBJECTID") # The street with the highest and the lowest parking spots are retrieved
-            st.pyplot(plotting(pivot_streets, "OBJECTID",
-                               'bar',
-                               'Major Streets and amount of Parking Lots',
-                               "# of parking spots by Major street",
-                               "*A Major Street is defined as having >= 100 parking spots",
-                               max = high,
-                               min = low,
-                               )) # Pivot table is plotted as a bar chart
-
-        case "Stacked Bar Chart": # if Stacked Bar Chart, then
-            pivot_meter = df.pivot_table(columns = "METER_TYPE",
-                                         index = "STREET",
-                                         aggfunc="size", # I used here Chatgpt
-                                         fill_value=0) # pivot table displays the number of parking spots per street by meter type
-            pivot_meter = pivot_meter[pivot_meter["MULTI-SPACE STALL"]>0] # we are only considering streets with at least a multi-space for our analysis
-            pivot_meter["COMBO"] = pivot_meter["MULTI-SPACE STALL"] + pivot_meter["SINGLE-SPACE"] # creating a new column combining the parking spots to a total
-            pivot_meter = pivot_meter.sort_values(by = "COMBO", ascending = False) # we are sorting the total in descending order
-            pivot_meter = pivot_meter[:5] # retrieving the 5 major streets (by size) with multipace-stalls on them
-            pivot_meter = filter_for_empty_and_useless(pivot_meter,"COMBO")
-            st.pyplot(plotting(pivot_meter, None,
-                               'stacked',
-                               '5 Largest Streets by Parking Lot Type',
-                               y_label= "Street Name",
-                               x_label= '(only streets with at least 1 MULTI_SPACE are considered)'
-                               )) # plotting the horizontal and stacked bar chart
+    else: # if Stacked Bar Chart, then
+        pivot_meter = df.pivot_table(columns = "METER_TYPE", index = "STREET", aggfunc="size", # I used here Chatgpt fill_value=0) # pivot table displays the number of parking spots per street by meter type
+        pivot_meter = pivot_meter[pivot_meter["MULTI-SPACE STALL"]>0] # we are only considering streets with at least a multi-space for our analysis
+        pivot_meter["COMBO"] = pivot_meter["MULTI-SPACE STALL"] + pivot_meter["SINGLE-SPACE"] # creating a new column combining the parking spots to a total
+        pivot_meter = pivot_meter.sort_values(by = "COMBO", ascending = False) # we are sorting the total in descending order
+        pivot_meter = pivot_meter[:5] # retrieving the 5 major streets (by size) with multipace-stalls on them
+        pivot_meter = filter_for_empty_and_useless(pivot_meter,"COMBO")
+        st.pyplot(plotting(pivot_meter, None,'stacked','5 Largest Streets by Parking Lot Type', y_label= "Street Name", x_label= '(only streets with at least 1 MULTI_SPACE are considered)')) # plotting the horizontal and stacked bar chart
 
 main()
 
